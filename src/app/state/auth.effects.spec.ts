f243640fab52ad92c5d84c12d77d5675ab511d1c
Marko Grešak <marko@gresak.io>
2021-03-11 12:04:31 +0100

    Add tests for updateAuthGrant effect

--- src/app/state/auth.effects.spec.ts
+++ src/app/state/auth.effects.spec.ts
@@ -8,11 +8,15 @@ import {
   routerNavigationAction,
   routerReducer,
 } from '@ngrx/router-store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
+import { AuthService } from '../auth.service';
+import { mockAuthGrant } from '../fixtures/mockAuthGrant';
+import { StorageService } from '../storage.service';
+import { AuthGrant } from '../types';
 import { AuthEffects } from './auth.effects';
 import { payload as routerNavigatedActionPayload } from './fixtures/router-navigated-action';
-import { updateAuthGrant } from './auth.actions';
+import { updateAuthGrant, updateAuthGrantSuccess } from './auth.actions';
 import { AppState } from '.';
 
 describe('AuthEffects', () => {
@@ -20,6 +24,8 @@ describe('AuthEffects', () => {
   let effects: AuthEffects;
   let store: MockStore<Partial<AppState>>;
   let testScheduler: TestScheduler;
+  let authService: jasmine.SpyObj<AuthService>;
+  let storageService: jasmine.SpyObj<StorageService<AuthGrant>>;
 
   const routeId = 'abc123';
   const initialState = {
@@ -32,9 +38,21 @@ describe('AuthEffects', () => {
   };
 
   beforeEach(() => {
+    const authServiceSpy = jasmine.createSpyObj<AuthService>('AuthService', [
+      'createMagicLink',
+    ]);
+    authServiceSpy.createMagicLink.and.returnValue(of(mockAuthGrant));
+
+    const storageServiceSpy = jasmine.createSpyObj<StorageService<AuthGrant>>(
+      'StorageService',
+      ['get', 'set', 'remove'],
+    );
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: StorageService, useValue: storageServiceSpy },
         AuthEffects,
         provideMockStore<Partial<AppState>>({
           initialState,
@@ -44,6 +62,8 @@ describe('AuthEffects', () => {
     });
 
     store = TestBed.inject(MockStore);
+    authService = TestBed.inject(AuthService) as typeof authServiceSpy;
+    storageService = TestBed.inject(StorageService) as typeof storageServiceSpy;
     effects = TestBed.inject(AuthEffects);
     testScheduler = new TestScheduler((actual, expected) => {
       expect(actual).toEqual(expected);
@@ -64,6 +84,39 @@ describe('AuthEffects', () => {
 
       expectObservable(effects.startUpdateAuthGrant$).toBe('(-b)', {
         b: updateAuthGrant({ id: routeId }),
+      });
+    });
+  });
+
+  it('should call updateAuthGrantSuccess action after receiving updateAuthGrant', () => {
+    testScheduler.run(({ hot, expectObservable, flush }) => {
+      actions$ = hot('(a-)', {
+        a: updateAuthGrant({ id: routeId }),
+      });
+
+      expectObservable(effects.updateAuthGrant$).toBe('(-b)', {
+        b: updateAuthGrantSuccess({ authGrant: mockAuthGrant }),
+      });
+      flush();
+      expect(authService.createMagicLink).toHaveBeenCalledOnceWith(routeId);
+    });
+  });
+
+  it('should use stored authGrant instead of calling createMagicLink', () => {
+    testScheduler.run(({ hot, expectObservable }) => {
+      const storedAuthGrant: AuthGrant = {
+        ...mockAuthGrant,
+        access_token: 'stored-access-token',
+        created_at: Math.floor(Date.now() / 1000),
+      };
+      storageService.get.and.returnValue(storedAuthGrant);
+
+      actions$ = hot('(a-)', {
+        a: updateAuthGrant({ id: routeId }),
+      });
+
+      expectObservable(effects.updateAuthGrant$).toBe('(-b)', {
+        b: updateAuthGrantSuccess({ authGrant: storedAuthGrant }),
       });
     });
   });
